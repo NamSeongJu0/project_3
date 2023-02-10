e9bc15082e1d4f25077700e32b97ca42157b9ead
NamSeongJu0 <skaehdwn0@naver.com>
2023-02-10 13:20:54 +0900

    0209
    ...ssss

--- jsfile/index.js
+++ jsfile/index.js
@@ -13,10 +13,13 @@ $(document).ready(function() {
         var lis = document.getElementsByClassName("bookinfo");
     
         for(var i = 0; i < lis.length; i++) {
+
+            var str = msg.documents[i].title;
+            var str2 = str.substring(0, 20);
     
             $(".bookinfo .img_1").eq(i).append("<img src = '" + msg.documents[i].thumbnail + "'/>");
     
-            $(".bookinfo .info").eq(i).append("<h4>" + msg.documents[i].title + "</h4>").append("<span>" + msg.documents[i].authors + "</span>").append("<p>" + msg.documents[i].price + "원" +  "</p>").append("<p>" + msg.documents[i].status + "</p>");
+            $(".bookinfo .info").eq(i).append("<h4>" + str2 + "</h4>").append("<span>" + msg.documents[i].authors + "</span>").append("<p>" + "정가 : " + msg.documents[i].price + "원" +  "</p>").append("<p>" + msg.documents[i].datetime + "</p>");
     
     
             $(".bookinfo").eq(i).find(".spen1").append("ADD TO CART");
@@ -34,27 +37,26 @@ $(document).ready(function() {
     $.ajax({
         method: "GET",    
         url: "https://dapi.kakao.com/v3/search/book?target=title",    
-        data: {query: "요리"},    
+        data: {query: "미래"},    
         headers: {Authorization: "KakaoAK 6f9bb4da749b9b559e49e18decbe669a"}
     })
     
     .done(function(msg) {
-        console.log(msg)
         var lis = document.getElementsByClassName("viewinfo");
     
         for(var i = 0; i < lis.length; i++) {
+
+            var str = msg.documents[i].title;
+            var str2 = str.substring(0, 20);
     
-            $(".viewinfo").eq(i).append("<img src = '" + msg.documents[i].thumbnail + "'/>");
+            $(".viewinfo .img_1").eq(i).append('<a href="#">' + "<img src = '" + msg.documents[i].thumbnail + "'/>" + '</a>');
     
-            $(".viewinfo").eq(i).append("<h4>" + msg.documents[i].title + "</h4>").append("<span>" + msg.documents[i].authors + "</span>");
-    
-    
-            var str = msg.documents[i].contents;
-            var str2 = str.substring(2, 55);
+            $(".viewinfo .info").eq(i).append("<h4>" + str2 + "</h4>").append("<span>" + msg.documents[i].authors + "</span>").append("<p>" + "정가 : "+ msg.documents[i].price + "원" +  "</p>").append("<p>" + msg.documents[i].datetime + "</p>");
     
     
-            $(".viewinfo").eq(i).append("<p>" + str2 + "</p>");
-            $(".viewinfo").eq(i).find(".img").append('<img src = "indeximg/my2.png" />', '<img src = "indeximg/my5.png"/>');
+            $(".viewinfo").eq(i).find(".spen1").append('<img src = "indeximg/my4.png" />');
+            
+            $(".viewinfo").eq(i).find(".img_2").append('<img src = "indeximg/my2.png" />', '<img src = "indeximg/my5.png"/>');
     
         }
 
@@ -67,10 +69,25 @@ $(document).ready(function() {
 
 $(document).ready(function(){
 
-    $('.bookinfo').hover(function() {
-        $(this).children('.info').css({"transition": "all 0.5s ease", "transform": "translateY(-20px)"});
+    $('.bookinfo, .viewinfo').hover(function() {
+        $(this).children('.img_1, .info').css({"transition": "all 0.5s ease", "transform": "translateY(-30px)"});
+        
+        $(this).css({"border": "1px solid #000000", "box-shadow": "4px 4px 4px #afafaf"});
+
     }, function() {
-        $(this).children('.info').css({"transition": "all 0.5s ease", "transform": "translateY(0px)"});
+        $(this).children('.img_1, .info').css({"transition": "all 0.5s ease", "transform": "translateY(0px)"});
+        
+        $(this).css({"border": "1px solid #afafaf", "box-shadow": "none"});    
+    });
+
+    
+    // 이미지 오버 효과
+    $(".spen1 img").mousemove(function(event) {
+        var x = event.pageX;
+        var y = event.pageY;
+        $(".click").css({left: x +18 , top: y -30}).addClass("on");
+    }).mouseleave(function() {
+        $(".click").removeClass("on");
     });
 
 });
