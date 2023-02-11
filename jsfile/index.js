61c53b1f4d34c80c35272b91eb3b21df92d47a2d
NamSeongJu0 <119376125+NamSeongJu0@users.noreply.github.com>
2023-02-12 01:29:47 +0900

    0212
    ...

--- jsfile/index.js
+++ jsfile/index.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
     
             $(".bookinfo .img_1").eq(i).append('<a href="#">' + "<img src = '" + msg.documents[i].thumbnail + "'/>" + "</a>");
     
-            $(".bookinfo .info").eq(i).append("<h4>" + str2 + "</h4>").append("<span>" + msg.documents[i].authors + "</span>").append("<p>" + "정가 : " + msg.documents[i].price + "원" +  "</p>").append("<p>" + msg.documents[i].refresh_token_expires_in + "</p>");
+            $(".bookinfo .info").eq(i).append("<h4>" + str2 + "</h4>").append("<span>" + msg.documents[i].authors + "</span>").append("<p>" + "정가 : " + msg.documents[i].price + "원" +  "</p>").append("<p>" + msg.documents[i].publisher + "</p>");
     
     
             $(".bookinfo").eq(i).find(".spen1").append("ADD TO CART");
@@ -42,21 +42,81 @@ $(document).ready(function() {
     })
     
     .done(function(msg) {
-        var lis = document.getElementsByClassName("viewinfo");
+        var lis = document.getElementsByClassName("futureinfo");
     
         for(var i = 0; i < lis.length; i++) {
 
             var str = msg.documents[i].title;
             var str2 = str.substring(0, 20);
     
-            $(".viewinfo .img_1").eq(i).append('<a href="#">' + "<img src = '" + msg.documents[i].thumbnail + "'/>" + "</a>");
+            $(".futureinfo .img_1").eq(i).append('<a href="#">' + "<img src = '" + msg.documents[i].thumbnail + "'/>" + "</a>");
     
-            $(".viewinfo .info").eq(i).append("<h4>" + str2 + "</h4>").append("<span>" + msg.documents[i].authors + "</span>").append("<p>" + "정가 : "+ msg.documents[i].price + "원" +  "</p>").append("<p>" + msg.documents[i].datetime + "</p>");
+            $(".futureinfo .info").eq(i).append("<h4>" + str2 + "</h4>").append("<span>" + msg.documents[i].authors + "</span>").append("<p>" + "정가 : "+ msg.documents[i].price + "원" +  "</p>").append("<p>" + msg.documents[i].publisher + "</p>");
     
     
-            $(".viewinfo").eq(i).find(".spen1").append('<img src = "indeximg/my4.png" />');
+            $(".futureinfo").eq(i).find(".spen1").append('<img src = "indeximg/my4.png" />');
             
-            $(".viewinfo").eq(i).find(".img_2").append('<img src = "indeximg/my2.png" />', '<img src = "indeximg/my5.png"/>');
+            $(".futureinfo").eq(i).find(".img_2").append('<img src = "indeximg/my2.png" />', '<img src = "indeximg/my5.png"/>');
+    
+        }
+
+    });
+
+
+    // sale 부분
+    $.ajax({
+        method: "GET",    
+        url: "https://dapi.kakao.com/v3/search/book?target=title",    
+        data: {query: "세일"},    
+        headers: {Authorization: "KakaoAK 6f9bb4da749b9b559e49e18decbe669a"}
+    })
+    
+    .done(function(msg) {
+        var lis = document.getElementsByClassName("saleinfo");
+    
+        for(var i = 0; i < lis.length; i++) {
+
+            var str = msg.documents[i].title;
+            var str2 = str.substring(0, 20);
+    
+            $(".saleinfo .img_1").eq(i).append('<a href="#">' + "<img src = '" + msg.documents[i].thumbnail + "'/>" + "</a>");
+    
+            $(".saleinfo .info").eq(i).append("<h4>" + str2 + "</h4>").append("<span>" + msg.documents[i].authors + "</span>").append("<p>" + "판매가 : "+ msg.documents[i].sale_price + "원" +  "</p>").append("<p>" + msg.documents[i].publisher + "</p>");
+    
+    
+            $(".saleinfo").eq(i).find(".spen1").append('<img src = "indeximg/my4.png" />');
+            
+            $(".saleinfo").eq(i).find(".img_2").append('<img src = "indeximg/my2.png" />', '<img src = "indeximg/my5.png"/>');
+    
+        }
+
+    });
+
+
+     // mood 부분
+     $.ajax({
+        method: "GET",    
+        url: "https://dapi.kakao.com/v3/search/book?target=title",    
+        data: {query: "기분"},    
+        headers: {Authorization: "KakaoAK 6f9bb4da749b9b559e49e18decbe669a"}
+    })
+    
+    .done(function(msg) {
+        var lis = document.getElementsByClassName("moodinfo");
+    
+        for(var i = 0; i < lis.length; i++) {
+
+            var str = msg.documents[i].title;
+            var str2 = str.substring(0, 20);
+    
+            $(".moodinfo .img_1").eq(i).append('<a href="#">' + "<img src = '" + msg.documents[i].thumbnail + "'/>" + "</a>");
+    
+            $(".moodinfo .info").eq(i).append("<h4>" + str2 + "</h4>").append("<span>" + msg.documents[i].authors + "</span>").append("<p>" + "판매가 : "+ msg.documents[i].sale_price + "원" +  "</p>").append("<p>" + msg.documents[i].publisher + "</p>");
+    
+    
+            $(".moodinfo").eq(i).find(".spen1").append('<img src = "indeximg/my4.png" />');
+            
+            $(".moodinfo").eq(i).find(".img_2").append('<img src = "indeximg/my2.png" />', '<img src = "indeximg/my5.png"/>');
     
         }
 
@@ -65,11 +125,11 @@ $(document).ready(function() {
 
 
 
-// booklist hover 효과
+// list들 hover 효과
 
 $(document).ready(function(){
 
-    $('.bookinfo, .viewinfo').hover(function() {
+    $('.bookinfo, .futureinfo, .saleinfo, .moodinfo').hover(function() {
         $(this).children('.img_1, .info').css({"transition": "all 0.5s ease", "transform": "translateY(-30px)"});
         
         $(this).css({"border": "1px solid #000000", "box-shadow": "4px 4px 4px #afafaf"});
@@ -82,12 +142,13 @@ $(document).ready(function(){
 
     
     // 이미지 오버 효과
-    $(".viewinfo .spen1").hover(function() {
+    $(".futureinfo .spen1, .saleinfo .spen1, .moodinfo .spen1").hover(function() {
         $(this).children('.click').stop().animate({
             height: '100%',
             opacity: "1",
             top: "0"
         }, 300);
+
     }, function() {
         $(this).children('.click').stop().animate({
             height: '0%',
@@ -97,4 +158,46 @@ $(document).ready(function(){
     });
 
 });
+
+
+// 클릭 효과
+$(document).ready(function() {
+
+    // 클릭 시 list 부분 나오는 효과
+    $(".salelist, .moodlist").css("display", "none");
+    $(".view li:nth-of-type(1)").css("color", "#dbc3c2")
+
+    $('.view li:nth-of-type(1)').click(function(){
+        $(this).css("color", "#dbc3c2");
+        $('.view li:nth-of-type(2), .view li:nth-of-type(3)').css("color", "#000000");
+
+        $('.futurelist').stop().fadeIn(900).css('display', 'flex');
+        $('.salelist').css('display', 'none').fadeOut();
+        $('.moodlist').css('display', 'none').fadeOut();
+    });
+
+    $('.view li:nth-of-type(2)').click(function(){
+        $(this).css("color", "#dbc3c2");
+        $('.view li:nth-of-type(1), .view li:nth-of-type(3)').css("color", "#000000");
+
+        $('.futurelist').css('display', 'none');
+        $('.salelist').stop().fadeIn(900).css('display', 'flex');
+        $('.moodlist').css('display', 'none');
+    });
+
+    $('.view li:nth-of-type(3)').click(function(){
+        $(this).css("color", "#dbc3c2");
+        $('.view li:nth-of-type(1), .view li:nth-of-type(2)').css("color", "#000000");
+
+        $('.futurelist').css('display', 'none');
+        $('.salelist').css('display', 'none');
+        $('.moodlist').stop().fadeIn(900).css('display', 'flex');
+    });
+
+    // $('.view li').hover(function() {
+    //     $(this).css({"color": "#dbc3c2", "transition": "all 0.5s ease", "cursor": "pointer"});
+    // }, function() {
+    //     $(this).css({"color": "#000000", "transition": "none", "cursor": "pointer"});
+    // });
+})
 
