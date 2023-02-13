f3e51cc16229dc2c6eb3e5b6dfa7b26d94d97e28
NamSeongJu0 <skaehdwn0@naver.com>
2023-02-13 13:21:31 +0900

    asdfa
    awefqdazd

--- jsfile/index.js
+++ jsfile/index.js
@@ -141,11 +141,51 @@ $(document).ready(function() {
     
             $(".list_box .list_img").eq(i).append('<a href="#">' + "<img src = '" + msg.documents[i].thumbnail + "'/>" + "</a>");
     
-            $(".list_box .list_info").eq(i).append("<h4>" + str2 + "</h4>").append("<span>" + msg.documents[i].authors + "</span>").append("<p>" + "판매가 : "+ msg.documents[i].sale_price + "원" +  "</p>").append("<p>" + msg.documents[i].publisher + "</p>");
-    
+            $(".list_box .list_info").eq(i).prepend("<p>" + "세일 마감까지 남은 시간" + "</p>").prepend("<p>" + "판매가 : "+ msg.documents[i].sale_price + "원" +  "</p>").prepend("<span>" + msg.documents[i].authors + "</span>").prepend("<h4>" + str2 + "</h4>").prepend("<p>" + msg.documents[i].publisher + "</p>");
         }
+    });    
+});
 
-    });
+
+
+// Deals 타이머 부분
+$(document).ready(function() {
+    const countDownTimer = function (id, date) {
+        var _vDate = new Date(date);
+        var _second = 1000;
+        var _minute = _second * 60;
+        var _hour = _minute * 60;
+        var _day = _hour * 24;
+        var timer;
+    
+        function showRemaining() {
+            var now = new Date();
+            var distDt = _vDate - now;
+    
+            var days = Math.floor(distDt / _day);
+            var hours = Math.floor((distDt % _day) / _hour);
+            var minutes = Math.floor((distDt % _hour) / _minute);
+            var seconds = Math.floor((distDt % _minute) / _second);
+    
+            document.getElementById(id).textContent = days + '일 ';
+            document.getElementById(id).textContent += hours + '시간 ';
+            document.getElementById(id).textContent += minutes + '분 ';
+            document.getElementById(id).textContent += seconds + '초';
+        }
+    
+        timer = setInterval(showRemaining, 1000);
+    }
+    
+    var dateObj = new Date();
+    dateObj.setDate(dateObj.getDate() + 1);
+    
+    
+    countDownTimer('day1', '07/20/2024 00:00 AM');
+    countDownTimer('day2', '03/20/2024 00:00 AM');
+    countDownTimer('day3', '02/20/2024 00:00 AM');
+    countDownTimer('day4', '07/20/2023 00:00 AM');
+    countDownTimer('day5', '07/20/2023 00:00 AM');
+    countDownTimer('day6', '01/20/2026 00:00 AM');
 });
 
 
