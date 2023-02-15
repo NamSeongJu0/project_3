c1dc5cad16014467c5f2d5e2d64a99ecee669be2
NamSeongJu0 <skaehdwn0@naver.com>
2023-02-15 16:47:57 +0900

    0215
    망할 스와이퍼~~~

--- jsfile/swiper.js
+++ jsfile/swiper.js
@@ -86,11 +86,6 @@ $(document).ready(function () {
         spaceBetween: 30,
         freeMode: true,
 
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
