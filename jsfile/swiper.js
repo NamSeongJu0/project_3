c342bd7f62853905febac1f1b778db96beb51c36
NamSeongJu0 <skaehdwn0@naver.com>
2023-02-15 15:58:22 +0900

    0215
    stdgxh

--- jsfile/swiper.js
+++ jsfile/swiper.js
@@ -1,46 +1,100 @@
-$(document).ready(function() {
+// swiper1
+$(document).ready(function () {
 
     // swiper1
-    var swiper = new Swiper(".mySwiper", {
+    var swiper = new Swiper(".mySwiper1", {
+
+        spaceBetween: 30,
+        effect: "fade",
+
+        loop: true,
+
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+        },
+
+
+        // navigation: {
+        //   nextEl: ".swiper-button-next",
+        //   prevEl: ".swiper-button-prev",
+        // },
+
+        // pagination: {
+        //   el: ".swiper-pagination",
+        //   clickable: true,
+        // },
+    });
+});
+
+
+
+// swiper2
+$(document).ready(function () {
+
+    // swiper1
+    var swiper = new Swiper(".mySwiper2", {
         slidesPerView: 2,
         spaceBetween: 10,
 
-        // autoplay: {
-        //     delay: 2500,
-        //     disableOnInteraction: false,
-        // },
+        loop: true,
 
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
     });
+});
 
 
 
-    // swiper2
-    var swiper = new Swiper(".mySwiper1", {
+// swiper3
+$(document).ready(function () {
+
+    var swiper = new Swiper(".mySwiper3", {
+
         effect: "coverflow",
         grabCursor: true,
         centeredSlides: true,
-
-        spaceBetween: 15,
-
-        slideToClickedSlide : true,
+        slidesPerView: "auto",
 
         coverflowEffect: {
-        //   rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
         },
 
         pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+});
+
+
+
+// swiper4
+$(document).ready(function () {
+
+    var swiper = new Swiper(".mySwiper4", {
+
+
+        grabCursor: true,
+        slidesPerView: 5,
+        spaceBetween: 30,
+        freeMode: true,
+
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
         },
 
-      });
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
 
-})
+    });
+});
