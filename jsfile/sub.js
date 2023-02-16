9a419eef9df8807c48233cce351dddad9b75dac9
NamSeongJu0 <skaehdwn0@naver.com>
2023-02-16 20:13:08 +0900

    0216
    sub 시작...

--- jsfile/sub.js
+++ jsfile/sub.js
@@ -3,27 +3,39 @@ $(document).ready(function() {
     $.ajax({
         method: "GET",
         url: "https://dapi.kakao.com/v3/search/book?target=title",
-        data: { query: "천년 여우 여우비" },
+        data: { query: "사진" },
         headers: { Authorization: "KakaoAK 6f9bb4da749b9b559e49e18decbe669a" }
     })
 
         .done(function (msg) {
 
-            var lis = document.getElementsByClassName("fox_book");
+            var lis = document.getElementsByClassName("photo_book");
 
             for (var i = 0; i < lis.length; i++) {
 
                 var str = msg.documents[i].title;
                 var str2 = str.substring(0, 20);
 
-                $(".fox_book .fox_img").eq(i).append('<a href="#">' + "<img src = '" + msg.documents[i].thumbnail + "'/>" + "</a>");
+                var str1 = msg.documents[i].contents;
+                var str3 = str1.substring(0, 170);
 
-                $(".fox_book .subinfo").eq(i).prepend("<h1>" + str2 + "</h1>");
+                $(".photo_book .subinfo").eq(i).append("<br>" + "<p>" + "정가 : " + msg.documents[i].price + "원" + "</p>").append("<p>" + "판매가 : " + msg.documents[i].sale_price + "원" + "</p>").prepend("<h2>" + msg.documents[i].authors + "</h2>" + "<br>").prepend("<h1>" + str2 + "</h1>");
+
+                // $(".img_text .publisher").append("<h4>" + msg.documents[i].publisher + "</h4>")
 
 
-                $(".sb_box").eq(i).find(".story").append("<p>" + msg.documents[i].contents + "</p>");
-                console.log("story")
+                $(".sb_box .story").eq(i).append("<p>" + str3 + "</p>");
+
             }
 
         });
+});
+
+
+
+// +, - 클릭 시 효과
+$(document).ready(function() {
+    $(".btn").click(function() {
+        $(this).css("transform", "scale(1.2)");
+    });
 });
